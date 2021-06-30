415595724ccce9ee1fea4fb4555ef816acf40005
NamCrong <86723759+NamCrong@users.noreply.github.com>
2021-06-30 20:20:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
+    introTitle: 'Bạn hiền ơi hôm nay trời đẹp lạ thường!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Mà sao mình cứ hay buồn
+    Đằng sau chữ nhậu đây là bạn`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Phải chăng tớ đã muốn nhậu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng tớ đã say ngay từ lúc cầm chén ',
+    btnYes: 'OK luôn bạn hiền ạ <33',
+    btnNo: 'rượu bia có hại cho sức khỏe :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại đồng ý đi nhậu với tớ <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Vì cậu nhạu zui zẻ zăn minh <33333333',
+    mess: 'Tớ biết rồi mà 🥰. không say không zề 😘😘',
+    messDesc: 'Tối nay 7h tớ qua qua đón nhé bạn hiền.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com'https://www.facebook.com/pororo1409 //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
